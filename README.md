1ea31814ba0ac9b4c05fd208c0173fe5b814c4d0
dead-horse <dead_horse@qq.com>
2014-10-10 16:23:39 +0800

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+urllib-sync
+---------------
+
+[![NPM version][npm-image]][npm-url]
+[![build status][travis-image]][travis-url]
+[![Test coverage][coveralls-image]][coveralls-url]
+[![David deps][david-image]][david-url]
+[![node version][node-image]][node-url]
+[![Gittip][gittip-image]][gittip-url]
+
+[npm-image]: https://img.shields.io/npm/v/urllib-sync.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/urllib-sync
+[travis-image]: https://img.shields.io/travis/node-modules/urllib-sync.svg?style=flat-square
+[travis-url]: https://travis-ci.org/node-modules/urllib-sync
+[coveralls-image]: https://img.shields.io/coveralls/node-modules/urllib-sync.svg?style=flat-square
+[coveralls-url]: https://coveralls.io/r/node-modules/urllib-sync?branch=master
+[david-image]: https://img.shields.io/david/node-modules/urllib-sync.svg?style=flat-square
+[david-url]: https://david-dm.org/node-modules/urllib-sync
+[node-image]: https://img.shields.io/badge/node.js-%3E=_0.10-green.svg?style=flat-square
+[node-url]: http://nodejs.org/download/
+[gittip-image]: https://img.shields.io/gittip/dead-horse.svg?style=flat-square
+[gittip-url]: https://www.gittip.com/dead-horse/
+
+a hack wrap to support sync http request
+
+## Installation
+
+```bash
+$ npm install urllib-sync
+```
+
+## Usage
+
+### License
+
+MIT
---
+++ test/index.test.js
@@ -0,0 +1,53 @@
+/*!
+ * urllib-sync - test/index.test.js
+ * Copyright(c) 2014 dead_horse <dead_horse@qq.com>
+ * MIT Licensed
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var urllib = require('..');
+var iconv = require('iconv-lite');
+
+describe('urllib-sync', function () {
+  describe('request()', function () {
+    it('should request gbk ok', function () {
+      var res = urllib.request('http://www.tmall.com');
+      var data = iconv.decode(res.data, 'gbk');
+      data.should.containEql('天猫');
+      res.status.should.equal(200);
+    });
+
+    it('should request text ok', function () {
+      var res = urllib.request('http://www.baidu.com', {
+        dataType: 'text'
+      });
+      res.data.should.containEql('百度');
+      res.status.should.equal(200);
+    });
+
+    it('should request json ok', function () {
+      var res = urllib.request('http://registry.npm.taobao.org/koa', {
+        dataType: 'json'
+      });
+      res.data.name.should.equal('koa');
+      res.status.should.equal(200);
+    });
+
+    it('should timeout', function () {
+      try {
+        urllib.request('http://www.baidu.com', {
+          dataType: 'text',
+          timeout: 10
+        });
+        throw new Error('should not exec');
+      } catch (err) {
+        err.message.should.containEql('timeout');
+      }
+    });
+  });
+});
